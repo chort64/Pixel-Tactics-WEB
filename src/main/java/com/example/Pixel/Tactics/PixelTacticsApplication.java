fff06e0d003182fba21686eab9c9ee2d3db0f9a8
chort64 <uhov.164@gmail.com>
2022-09-16 16:34:46 +0400

    beginning

---
+++ src/main/java/com/example/Pixel/Tactics/PixelTacticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pixel.Tactics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelTacticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelTacticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pixel/Tactics/PixelTacticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pixel.Tactics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelTacticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
